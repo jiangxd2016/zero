5b4742d27f364c28a0719977541d69e66037d76c
jiangxd <jiangxd2016@gmail.com>
2023-04-19 10:46:27 +0800

    chore: remove old name project

--- README.md
+++
@@ -1,4 +0,0 @@
-# @estjs/zero
-
-[![NPM version](https://img.shields.io/npm/v/@estjs/zero?color=a1b858&label=)](https://www.npmjs.com/package/@estjs/zero)
-
--- example/src/store/index.ts
+++
@@ -1,8 +0,0 @@
-import useUserStore from './modules/user';
-import permissionStore from './modules/permission';
-import tabBarStore from './modules/tab-bar';
-export {
-  useUserStore,
-  permissionStore,
-  tabBarStore
-};
